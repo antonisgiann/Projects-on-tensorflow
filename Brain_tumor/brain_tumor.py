bc4bf73de138c33976db0676762e629ffa7edd18
carnivore7777 <antgiannoulopoulos@gmail.com>
2023-04-07 11:50:13 +0100

    Updating model

--- Brain_tumor/brain_tumor.py
+++ Brain_tumor/brain_tumor.py
@@ -3,18 +3,16 @@ import tensorflow as tf
 import sys
 sys.path.append("..")
 import pandas as pd
-import numpy as np
 import os
-from sklearn.model_selection import train_test_split
 from utils import plot_history
-from helper import brain_tumor_model
+from helper import brain_tumor_model, get_data_generators
 import opendatasets as od
 
 PROJECT_URL = "https://www.kaggle.com/datasets/fernando2rad/brain-tumor-mri-images-44c"
 PROJECT_NAME = PROJECT_URL.split("/")[-1]
 DATA_PATH = os.path.join("../datasets", PROJECT_NAME)
-BATCH_SIZE = 96
-IMG_SHAPE = (224,224,3)
+BATCH_SIZE = 64
+IMG_SHAPE = (456,456,3)
 
 od.download(PROJECT_URL, "../datasets")
 
@@ -29,28 +27,14 @@ metadata = pd.DataFrame({"filepath":files, "tumor":classes})
 # %%
 num_classes = len(os.listdir(DATA_PATH))
 
-X_train, X_valid = train_test_split(metadata, test_size=0.2, random_state=47, stratify=metadata["tumor"])
-X_valid, X_test = train_test_split(X_valid, test_size=0.5, random_state=47, stratify=X_valid["tumor"])
-
-train_gen = tf.keras.preprocessing.image.ImageDataGenerator(horizontal_flip=True).flow_from_dataframe(
-    X_train, x_col="filepath", y_col="tumor", target_size=(224,224), color_mode="rgb",
-    class_mode="categorical", batch_size=BATCH_SIZE, seed=47
-)
-valid_gen = tf.keras.preprocessing.image.ImageDataGenerator(horizontal_flip=True).flow_from_dataframe(
-    X_valid, x_col="filepath", y_col="tumor", target_size=(224,224), color_mode="rgb",
-    class_mode="categorical", batch_size=BATCH_SIZE, seed=47
-)
-test_gen = tf.keras.preprocessing.image.ImageDataGenerator(horizontal_flip=True).flow_from_dataframe(
-    X_test, x_col="filepath", y_col="tumor", target_size=(224,224), color_mode="rgb",
-    class_mode="categorical", batch_size=BATCH_SIZE, seed=47
-)
+train_gen, valid_gen, test_gen = get_data_generators(metadata, IMG_SHAPE[:-1], BATCH_SIZE)
 
 model = brain_tumor_model(IMG_SHAPE, num_classes)
 model.compile(optimizer=tf.keras.optimizers.Adamax(learning_rate=0.001),
               loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
               metrics=["accuracy"])
 
-history = model.fit(train_gen, epochs=20, validation_data=valid_gen)
+history = model.fit(train_gen, epochs=15, validation_data=valid_gen)
 
 plot_history((history.history["loss"],
               history.history["val_loss"],
@@ -58,14 +42,14 @@ plot_history((history.history["loss"],
               history.history["val_accuracy"]))
 
 # %% fine tuning
-for layer in model.layers[4].layers[int(len(model.layers[4].layers)*0.7):]:
+for layer in model.layers[2].layers[int(len(model.layers[2].layers)*0.7):]:
     layer.trainable = True
 
-model.compile(optimizer=tf.keras.optimizers.Adamax(learning_rate=0.0001),
+model.compile(optimizer=tf.keras.optimizers.Adamax(learning_rate=0.00015),
               loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
               metrics=["accuracy"])
 
-history2 = model.fit(train_gen, epochs=40, initial_epoch=history.epoch[-1], validation_data=valid_gen)
+history2 = model.fit(train_gen, epochs=30, initial_epoch=history.epoch[-1], validation_data=valid_gen)
 
 plot_history((history.history["loss"] + history2.history["loss"],
               history.history["val_loss"] + history2.history["val_loss"],
