f118e6fdb424ec206fffb6df5c9c04570f3d7445
carnivore7777 <antgiannoulopoulos@gmail.com>
2023-04-10 18:50:37 +0100

    Adding first draft of cifar10 project.

---
+++ Cifar10/cifar10.py
@@ -0,0 +1,53 @@
+# %%
+import sys
+sys.path.append("..")
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from helper import object_map, ModelWrapper, simple_dense_model, MyCallBack
+from utils import plot_history
+
+
+(X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
+X_valid, X_test, y_valid, y_test = train_test_split(X_test, 
+                                                    y_test, 
+                                                    test_size=0.5, 
+                                                    stratify=y_test, 
+                                                    random_state=47)
+
+BATCH_SIZE = 512
+IMG_SHAPE = X_train.shape[1:]
+
+
+plt.figure(figsize=(8,8))
+for i in range(9):
+    plt.subplot(3,3,i+1)
+    plt.title(object_map[y_train[i][0]])
+    plt.imshow(X_train[i])
+    plt.axis("off")
+plt.show()
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Rescaling(1./255),
+    simple_dense_model(shape=IMG_SHAPE)
+])
+
+model.compile(optimizer=tf.keras.optimizers.Adamax(learning_rate=0.001),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=["accuracy"])
+
+history = model.fit(X_train, 
+                    y_train, 
+                    batch_size=BATCH_SIZE, 
+                    epochs=20, 
+                    validation_data=(X_valid,y_valid),
+                    callbacks=[MyCallBack()]
+                    )
+
+plot_history(
+    (history.history["loss"],
+     history.history["val_loss"],
+     history.history["accuracy"],
+     history.history["val_accuracy"])
+)
+# %%
---
+++ Cifar10/helper.py
@@ -0,0 +1,87 @@
+import tensorflow as tf
+import numpy as np
+
+object_map = {
+    0: "Airplane",
+    1: "Automobile",
+    2: "Bird",
+    3: "Cat",
+    4: "Deer",
+    5: "dog",
+    6: "Frog",
+    7: "Horse",
+    8: "Ship",
+    9: "Truck"
+}
+
+class ModelWrapper():
+    def __init__(self, model):
+        self.model = model
+
+    @tf.function
+    def fit(self, x_train, y_train, epochs, batch_size, validation_data, loss, optimizer):
+        for i in range(epochs):
+            pass
+            # work in progress
+    def get_model(self):
+        return self.model
+    
+
+class MyCallBack(tf.keras.callbacks.Callback):
+    def __init__(self, lr_patience=3, stop_patience=5):
+        super(MyCallBack, self).__init__()
+        self.lr_patience = lr_patience
+        self.stop_patience = stop_patience
+        self.best_weights = None
+        self.best_acc = 0
+        self.lr_wait = 0
+        self.stop_wait = 0
+
+    def on_epoch_end(self, epoch, logs=None):
+        current_acc = logs.get("val_accuracy")
+        if current_acc > self.best_acc:
+            self.best_weights = self.model.get_weights()
+            self.best_acc = current_acc
+            self.lr_wait = 0
+            self.stop_wait = 0
+        else:
+            self.lr_wait += 1
+            self.stop_wait += 1
+            if self.lr_wait == self.lr_patience:
+                current_lr = tf.keras.backend.get_value(self.model.optimizer.learning_rate)
+                new_lr = current_lr + 0.2 * current_lr
+                tf.keras.backend.set_value(self.model.optimizer.learning_rate, new_lr)
+                print("\nThe learning rate has changed, old value was {0} and the new value is {1}".format(current_lr, new_lr))
+                self.lr_wait = 0
+            if self.stop_wait == self.stop_patience:
+                self.model.stop_training = True
+                self.model.set_weights(self.best_weights)
+
+
+def simple_dense_model(shape: tuple):
+        inputs = tf.keras.Input(shape=shape)
+        x = tf.keras.layers.Flatten(input_shape=shape)(inputs)
+        x = tf.keras.layers.Dense(500, 
+                                  activation="relu", 
+                                  kernel_regularizer=tf.keras.regularizers.l2(),
+                                  bias_regularizer=tf.keras.regularizers.l2())(x)
+        x = tf.keras.layers.Dense(500, 
+                                  activation="relu", 
+                                  kernel_regularizer=tf.keras.regularizers.l2(),
+                                  bias_regularizer=tf.keras.regularizers.l2())(x)
+        x = tf.keras.layers.Dense(500, 
+                                  activation="relu", 
+                                  kernel_regularizer=tf.keras.regularizers.l2(),
+                                  bias_regularizer=tf.keras.regularizers.l2())(x)
+        x = tf.keras.layers.Dense(500, 
+                                  activation="relu", 
+                                  kernel_regularizer=tf.keras.regularizers.l2(),
+                                  bias_regularizer=tf.keras.regularizers.l2())(x)
+        x = tf.keras.layers.Dense(500, 
+                                  activation="relu", 
+                                  kernel_regularizer=tf.keras.regularizers.l2(),
+                                  bias_regularizer=tf.keras.regularizers.l2())(x)
+        outputs = tf.keras.layers.Dense(10)(x)
+
+        return tf.keras.Model(inputs, outputs)    
+  
